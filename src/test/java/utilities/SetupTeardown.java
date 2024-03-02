81ad1926504db70738795b1610b7dc27327af758
udhaychandran <udhaychandran111@gmail.com>
2024-03-02 13:03:40 +0530

    Test

--- src/test/java/utilities/SetupTeardown.java
+++ src/test/java/utilities/SetupTeardown.java
@@ -20,35 +20,30 @@ public class SetupTeardown {
 	
 	public static ExtentReports extentReport;
 	public static ExtentTest extentTest;
-	
+	public static String className;
 	public static WebDriver driver;
 	public WebDriverWait wait;
 	public SetupTeardown() {
 		this.driver=driver;
 	}
-	@BeforeClass
+	
+	
+	@BeforeTest
 	public void setUp() {
-		try {
-			extentReport = new ExtentReports(System.getProperty("user.dir")+"\\\\ExtentReportResults.html");
-			extentTest = extentReport.startTest("SetupTeardown");
+				
 			WebDriverManager.chromedriver().clearDriverCache().setup();
 			driver = new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 			driver.manage().window().maximize(); 	
-						extentTest.log(LogStatus.PASS, "THe Browser is launched");
-		}
-		catch (Exception e) {
-			// TODO: handle exception
-			extentTest.log(LogStatus.FAIL, "Experiencigng an issue while launching the browser. Please Check!");
+			driver.get("https://the-internet.herokuapp.com/");
 		}
 		
-	}
 	
-//	@AfterClass
-//	public void tearDown() {
-//		driver.quit();
-//		System.out.println("After test executed");
-//	}
+ 	@AfterTest
+ 	public void tearDown() {
+ 		driver.quit();
+ 		System.out.println("After test executed");
+ 	}
 	
 	public void waitForSometime() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
