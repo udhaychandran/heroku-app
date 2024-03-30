6db5a670aee19432a1ec150728ad03a5c1471a9c
udhaychandran <udhaychandran111@gmail.com>
2024-03-30 19:24:18 +0530

    New test

--- src/test/java/helper/DropDown.java
+++ src/test/java/helper/DropDown.java
@@ -1,31 +1,45 @@
 package helper;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
+import utilities.Functions;
 import utilities.SetupTeardown;
 
 public class DropDown extends SetupTeardown{
 
+	Functions func = new Functions();
+	String folderName = "DropDown";
 	@Test
 	public void dropDown() {
 		
 		try
 		{
+			extentTest = extentReports.createTest("DropDown");
 			driver.findElement(By.linkText("Dropdown")).click();
 			Thread.sleep(2000);
 			Select select = new Select(driver.findElement(By.id("dropdown")));
 			List<WebElement> listDropDown = select.getOptions();
 			for(WebElement list:listDropDown)
 			{
-				System.out.println(list.getText());
+				if(list.getText().equals("Option 1")) {
+					list.click();
+				}
 			}
-			select.selectByVisibleText("Option 1");
-			Thread.sleep(2000);
+			func.takeScreenShot(driver, folderName, folderName, "OptionSelected");
+			extentTest.log(Status.PASS, "The option is selected");
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			WebElement selectedValue = select.getFirstSelectedOption();
+			String dropValue = selectedValue.getText();
+			System.out.println("The Selected value in dropdown is "+dropValue);
 		}
 		catch (Exception e) {
 			// TODO: handle exception
--- src/test/java/utilities/WaitClass.java
+++ src/test/java/utilities/WaitClass.java
@@ -3,6 +3,7 @@ package utilities;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,6 +18,11 @@ public class WaitClass extends SetupTeardown{
 	
 	public WebElement waitForElementToBeClicable(WebElement closeButton) {
 		return wait.until(ExpectedConditions.elementToBeClickable(closeButton));
+	}
+	
+	public void scrollBy() {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,500);");
 	}
 	
 }
